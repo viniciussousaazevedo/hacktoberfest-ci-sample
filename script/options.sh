a21b5270505bfca0f88d4d90d87f5d960857ba12
viniciussousaazevedo <sousa0240@gmail.com>
2023-10-08 10:07:18 -0300

    creates controller and TODO funcs

---
+++ script/options.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -uo pipefail
+
+create_contact() {
+    echo "TODO"
+}
+
+read_contact() {
+    echo "TODO"
+}
+
+update_contact() {
+    echo "TODO"
+}
+
+delete_contact() {
+    echo "TODO"
+}
+
+exit_program() {
+    echo -e "See you later!\n"
+    exit 0
+}
+
+invalid_option() {
+    echo -e "Invalid option, please try again.\n"
+}

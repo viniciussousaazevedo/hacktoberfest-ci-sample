03ef655081306d280a523b6dc15a2c69fd583ca9
viniciussousaazevedo <sousa0240@gmail.com>
2023-10-08 21:15:34 -0300

    features creation: update and deletion

--- script/options.sh
+++ script/options.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -uo pipefail
 
+contacts_contains() {
+    if jq '.[] | .name' contacts.json | grep -qw "$1"; then
+        return 0 # true
+    else
+        return 1 # false
+    fi
+}
+
 create_contact() {
     read -p "Inform the contact's name: " name
+    contacts_contains $name
 
-    if jq '.[] | .name' contacts.json | grep $name > /dev/null; then
+    if contacts_contains $name ; then
         echo -e "\nThis name already exists in your list of contacts.\n"
         return
     fi
@@ -24,7 +33,7 @@ create_contact() {
 read_contact() {
     read -p "Inform the contact's name: " name
 
-    if jq '.[] | .name' contacts.json | grep $name > /dev/null; then
+    if contacts_contains $name; then
         number=$(jq ".[] | select(.name==\"$name\") | .number" contacts.json)
         echo -e "Name: $name"
         echo -e "Number: $number\n"
@@ -34,11 +43,36 @@ read_contact() {
 }
 
 update_contact() {
-    echo "TODO"
+    read -p "Inform the contact's name: " name
+
+    if contacts_contains $name; then
+        read -p "Inform the new name of this contact: " new_name
+        read -p "Inform the new number of this contact: " new_number
+
+        # BUG: e se new_name já existir na lista?
+
+        contact=$(jq ".[] | select(.name==\"$name\") | .name = \"$new_name\" | .number = \"$new_number\"" contacts.json)
+        partial_list=$(jq "del(.[] | select(.name == \"$name\"))" contacts.json)
+        updated_list=$(echo $partial_list | jq ". += [$contact]")
+        echo $updated_list > contacts.json
+
+        echo -e "\nThis contact has been successfully updated.\n"
+    else
+        echo -e "This name does not exist in your list of contacts.\n"
+    fi
 }
 
 delete_contact() {
-    echo "TODO"
+    read -p "Inform the contact's name: " name
+
+    if contacts_contains $name; then
+        updated_list=$(jq "del(.[] | select(.name == \"$name\"))" contacts.json)
+        echo $updated_list > contacts.json
+
+        echo -e "\nThis contact has been successfully removed.\n"
+     else
+        echo -e "This name does not exist in your list of contacts.\n"
+    fi
 }
 
 exit_program() {
